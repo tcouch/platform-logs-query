0bdc9bc312c3d326e611de97c615d8e20fea7e3c
tcouch <t.couch@ucl.ac.uk>
2015-07-08 15:12:11 +0100

    Created separate nodeDict dictionary of node name mappings

---
+++ legion-logs-query/nodeDict.py
@@ -0,0 +1,8 @@
+nodeDict = {}
+nodeDict['W'] = 'node-[a-j]'
+nodeDict['X'] = 'node-[k-p]'
+nodeDict['Y'] = 'node-[0-1]'
+nodeDict['Z'] = 'node-2'
+nodeDict['V'] = 'node-3'
+nodeDict['U'] = 'node-[5rst]'
+nodeDict['T'] = 'node-6'
